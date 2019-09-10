9d409f4be572b5794cb7dacbb3d5a79d11239c91
stash hufsi <arnobodrib@gmail.com>
2019-09-10 14:35:11 +0600

    FirstAssignment

---
+++ FirstAssignment1/FirstAssignment1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "";
+            string Contact = "";
+            string Address = "";
+            int Age;
+
+            Console.WriteLine("Your Name?");
+            Name = Console.ReadLine();
+
+            Console.WriteLine( "What is your Age?  " );
+            Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your Contact Number?  ");
+            Contact = Console.ReadLine();
+
+            Console.WriteLine("Address? ");
+            Address = Console.ReadLine();
+
+            Console.WriteLine("My Name : " + Name);
+            Console.WriteLine("My Age : " +Age);
+            Console.WriteLine("My Phone Number :" + Contact);
+            Console.WriteLine("My Address :" + Address);
+
+            Console.ReadLine();
+
+        }
+    }
+}
